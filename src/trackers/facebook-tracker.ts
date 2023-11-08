b506ace415c9084dc4924fcde0c859a0d0b9c928
Harun Daloglu <harund@gmail.com>
2023-11-08 14:28:41 -0500

    facebook tracker troubleshoot

--- src/trackers/facebook-tracker.ts
+++ src/trackers/facebook-tracker.ts
@@ -22,19 +22,38 @@ export class FacebookTracker implements ITracker {
     if (document.getElementById("facebook-tracker-" + this.tag)) {
       return;
     }
+
     const fbScript = document.createElement("script");
-    fbScript.async = true;
-    fbScript.src = "https://connect.facebook.net/en_US/fbevents.js";
-    fbScript.innerHTML = "";
-    document.head.insertBefore(fbScript, document.head.childNodes[document.head.childNodes.length-1]);
-    fbq('init', this.tag);
-    fbq('track', 'PageView');
+    fbScript.innerHTML = `!function(f,b,e,v,n,t,s)     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?    n.callMethod.apply(n,arguments):n.queue.push(arguments)};    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';    n.queue=[];t=b.createElement(e);t.async=!0;    t.src=v;s=b.getElementsByTagName(e)[0];    s.parentNode.insertBefore(t,s)}(window, document,'script',    'https://connect.facebook.net/en_US/fbevents.js');    fbq('init', '${this.tag}');    fbq('track', 'PageView');`;
+    document.head.insertBefore(fbScript, document.head.childNodes[0]);
+    // <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=245936138167934&ev=PageView&noscript=1"/></noscript>
+
     const noScriptFb = document.createElement('noscript');
     noScriptFb.id = "facebook-tracker-" + this.tag;
-    noScriptFb.innerHTML = `<img height="1" width="1" style="display:none" 
+    noScriptFb.innerHTML = `<img height="1" width="1" style="display:none"
        src="https://www.facebook.com/tr?id=${this.tag}&ev=PageView&noscript=1"/>`;
     document.head.insertBefore(noScriptFb, document.head.childNodes[0]);
     fbq('track', 'ViewContent');
+
+
+
+
+
+
+
+    // const fbScript = document.createElement("script");
+    // fbScript.async = true;
+    // fbScript.src = "https://connect.facebook.net/en_US/fbevents.js";
+    // fbScript.innerHTML = "";
+    // document.head.insertBefore(fbScript, document.head.childNodes[document.head.childNodes.length-1]);
+    // fbq('init', this.tag);
+    // fbq('track', 'PageView');
+    // const noScriptFb = document.createElement('noscript');
+    // noScriptFb.id = "facebook-tracker-" + this.tag;
+    // noScriptFb.innerHTML = `<img height="1" width="1" style="display:none"
+    //    src="https://www.facebook.com/tr?id=${this.tag}&ev=PageView&noscript=1"/>`;
+    // document.head.insertBefore(noScriptFb, document.head.childNodes[0]);
+    // fbq('track', 'ViewContent');
   }
   doViewItem = (product: IProduct) => {
     this.doInstall();
